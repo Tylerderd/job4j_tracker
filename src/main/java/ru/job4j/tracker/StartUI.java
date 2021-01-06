103d9a5e408b175d8aaee47552f825a4fb844ebd
Tylerderd <simpskill38@gmail.com>
2021-01-07 01:33:23 +0300

    9. toString [#429139]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,12 +2,7 @@ package ru.job4j.tracker;
 
 public class StartUI extends Item {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        tracker.add(new Item("Kik"));
-        tracker.add(new Item("Bomb"));
-        tracker.add(new Item("Bottle"));
-        tracker.add(new Item("Car"));
-        tracker.add(new Item("Skiff"));
-        System.out.println(tracker.delete(4));
+        Item item = new Item(3, "Apple");
+        System.out.println(item.toString());
     }
 }
